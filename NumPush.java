237abbf0f6007ff49120da74ff02cb0e04d509ad
kulki-adi22 <67149382+kulki-adi22@users.noreply.github.com>
2023-05-29 20:23:59 +0530

    Create NumPush.java

---
+++ NumPush.java
@@ -0,0 +1,31 @@
+package com.example.myapplication;
+
+public class NumPush {
+    private String vehNum;
+    private String rName;
+    private String age;
+
+    public String getVehNum() {
+        return vehNum;
+    }
+
+    public void setVehNum(String vehNum) {
+        this.vehNum = vehNum;
+    }
+
+    public String getRName() {
+        return rName;
+    }
+
+    public void setRName(String rName) {
+        this.rName = rName;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
