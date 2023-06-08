b101fad9c6cf3b6c2b52f724cfd10c7857796337
kulki-adi22 <67149382+kulki-adi22@users.noreply.github.com>
2023-06-09 02:30:34 +0530

    Update MainActivityMain.java

--- MainActivityMain.java
+++ MainActivityMain.java
@@ -8,17 +8,25 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivityMain extends AppCompatActivity {
-    Button btn1;
+    Button btn1,btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_main);
-        btn1 = findViewById(R.id.btn1);
+        btn1 = findViewById(R.id.addbtn);
+        btn2 = findViewById(R.id.viewBtn);
         Intent intent = new Intent(MainActivityMain.this,MainActivity.class);
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(intent);
+            }
+        });
+        Intent intent1 = new Intent(MainActivityMain.this,ViewFiles.class);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent1);
             }
         });
     }
